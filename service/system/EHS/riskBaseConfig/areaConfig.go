6b2c9978bb1bd26c62531716f9e98ff575ec99dd
ciao-right <1115774759@qq.com>
2023-03-04 09:36:38 +0800

    table options

--- service/system/EHS/riskBaseConfig/areaConfig.go
+++ service/system/EHS/riskBaseConfig/areaConfig.go
@@ -3,6 +3,7 @@ package riskBaseService
 import (
 	"fmt"
 	"go_blog/common/global"
+	"go_blog/model/request"
 	"go_blog/model/request/riskBaseConfig"
 	"go_blog/utils"
 )
@@ -14,8 +15,15 @@ func (RiskService) AddAreaConfig(config riskBaseConfig.RiskAreaConfig) error {
 	return result.Error
 }
 
-func (RiskService) GetAreaConfigList(page, limit int, condition riskBaseConfig.RiskAreaConfig) (list []riskBaseConfig.RiskAreaConfig, err error) {
-	result := global.GLOBAL_DB.Preload("ResponsibleDepartment").Preload("ResponsiblePerson").Offset(utils.GetPage(page, limit)).Limit(limit).Find(&list, condition)
+type RiskAreaConfigRes struct {
+	riskBaseConfig.RiskAreaConfig
+	Dept request.Department `json:"responsible_department" gorm:"foreignKey:id;"`
+	User request.User       `json:"responsible_person" gorm:"foreignKey:id;"`
+}
+
+func (RiskService) GetAreaConfigList(page, limit int, condition riskBaseConfig.RiskAreaConfig) (list []RiskAreaConfigRes, err error) {
+
+	result := global.GLOBAL_DB.Preload("Dept", "state NOT IN (?)", 0).Preload("User").Offset(utils.GetPage(page, limit)).Limit(limit).Find(&list, condition)
 	fmt.Println(result)
 	return list, result.Error
 }
